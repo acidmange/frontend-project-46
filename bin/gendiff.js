f6ad4da6ff8d384e6e6dbc5b83a8445f361587a4
Alexander Atamanyuk <psotiframe13@gmail.com>
2023-12-10 21:23:15 +0500

    feat: add gendiff options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,4 +8,11 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0');
 
+program
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
+
 program.parse(process.argv);
+
+console.log('works');
