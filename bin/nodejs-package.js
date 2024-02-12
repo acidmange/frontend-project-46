b7ced0cf61c68f84dea0808a802b555fe3421b1b
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-02-12 15:53:45 +0500

    feat: add version and help commands

---
+++ bin/nodejs-package.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import genDiff from '../src/gendiff.js';
+
+genDiff();
+
---
+++ src/gendiff.js
@@ -0,0 +1,12 @@
+import { Command } from 'commander';
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information');
+
+program.parse();
+
+export default program;
