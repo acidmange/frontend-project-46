c7b5f392b2732d595b284b622f7e5e4bff8ef02d
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-02-16 14:41:00 +0500

    feat: add arguments

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,7 +5,10 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 
 program.parse();
 
