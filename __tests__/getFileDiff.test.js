7aee10828bd66d916d03aa5d6df3e8010100eafb
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-03-05 13:17:16 +0500

    refactor: add new tests

--- __tests__/getFileDiff.test.js
+++ __tests__/getFileDiff.test.js
@@ -193,10 +193,55 @@ const result6 = `{
     }
 }`;
 
+const result7 = `
+Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]`;
+
+const result8 = `
+Property 'common.follow' was removed
+Property 'common.setting2' was added with value: 200
+Property 'common.setting3' was updated. From null to true
+Property 'common.setting4' was removed
+Property 'common.setting5' was removed
+Property 'common.setting6.doge.wow' was updated. From 'so much' to ''
+Property 'common.setting6.ops' was removed
+Property 'group1.baz' was updated. From 'bars' to 'bas'
+Property 'group1.nest' was updated. From 'str' to [complex value]
+Property 'group2' was added with value: [complex value]
+Property 'group3' was removed`;
+
+const result9 = `
+Property 'env.browser' was updated. From true to false
+Property 'env.es2021' was removed
+Property 'env.jest/globals' was updated. From true to false
+Property '.extends' was updated. From 'airbnb-base' to 'babel-eslint'
+Property 'parserOptions.ecmaVersion' was updated. From 'latest' to 2018
+Property 'parserOptions.sourceType' was updated. From 'module' to 'script'
+Property 'plugins' was removed
+Property 'rules.consistent-return' was removed
+Property 'rules.import/extensions' was removed
+Property 'rules.no-console' was updated. From 0 to 1
+Property 'rules.no-underscore-dangle' was updated. From [complex value] to [complex value]`;
+
 test('filesDiff normal use', () => {
   expect(fileDiff(obj1, obj2, 'stylish')).toBe(result1);
   expect(fileDiff(obj2, obj1, 'stylish')).toBe(result2);
   expect(fileDiff(obj5, obj6, 'stylish')).toBe(result3);
+});
+
+test('filesDiff plain normal use', () => {
+  expect(fileDiff(obj1, obj2, 'plain')).toBe(result7);
+  expect(fileDiff(obj2, obj1, 'plain')).toBe(result8);
+  expect(fileDiff(obj5, obj6, 'plain')).toBe(result9);
 });
 
 test('filesDiff empty use', () => {
