ce89dd96906fb8a7461750f93bfb57270a3cec80
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-03-05 15:09:07 +0500

    refactor: change structure

--- bin/nodejs-package.js
+++ bin/nodejs-package.js
@@ -1,5 +1,28 @@
 #!/usr/bin/env node
 
-import program from '../src/gendiff.js';
+import { Command } from 'commander';
+import getFileDiff from '../src/getFileDiff.js';
+import getFileInfo from '../src/getFileInfo.js';
 
-program.parse();
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((pathA, pathB) => {
+    const fileA = getFileInfo(pathA);
+    const fileB = getFileInfo(pathB);
+    if (!fileA || !fileB) {
+      console.log('wrong input format');
+    } else {
+      const formatter = program.opts().format ?? 'stylish';
+      const result = getFileDiff(fileA, fileB, formatter);
+      console.log(result);
+    }
+  })
+  .parse(process.argv);
