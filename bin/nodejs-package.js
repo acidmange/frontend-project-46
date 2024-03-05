775be0fe57ecd356ecf83b85a766a6ea60bc2f23
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-03-05 14:05:12 +0500

    fix: fix program structure

--- bin/nodejs-package.js
+++ bin/nodejs-package.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import genDiff from '../src/gendiff.js';
+import program from '../src/gendiff.js';
 
-genDiff();
+program.parse();
--- src/gendiff.js
+++ src/gendiff.js
@@ -2,30 +2,26 @@ import { Command } from 'commander';
 import getFileInfo from './getFileInfo.js';
 import getFileDiff from './getFileDiff.js';
 
-const genDiffFunc = () => {
-  const program = new Command();
+const program = new Command();
 
-  program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1', '-V, --version', 'output the version number')
-    .helpOption('-h, --help', 'output usage information')
-    .option('-f, --format [type]', 'output format', 'stylish')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
-    .action((pathA, pathB) => {
-      const fileA = getFileInfo(pathA);
-      const fileB = getFileInfo(pathB);
-      if (!fileA || !fileB) {
-        console.log('wrong input format');
-      } else {
-        const formatter = program.opts().format ?? 'stylish';
-        const result = getFileDiff(fileA, fileB, formatter);
-        console.log(result);
-      }
-    });
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((pathA, pathB) => {
+    const fileA = getFileInfo(pathA);
+    const fileB = getFileInfo(pathB);
+    if (!fileA || !fileB) {
+      console.log('wrong input format');
+    } else {
+      const formatter = program.opts().format ?? 'stylish';
+      const result = getFileDiff(fileA, fileB, formatter);
+      console.log(result);
+    }
+  });
 
-  program.parse();
-};
-
-export default genDiffFunc;
+export default program;
